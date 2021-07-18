b0c9545e4a88e6d27ea246a98a480a20963a533d
regis-fontaine <regis.s.fontaine@gmail.com>
2021-07-18 20:48:15 +0200

    add last link WhitePaper

--- components/sections/Whitepaper.jsx
+++ components/sections/Whitepaper.jsx
@@ -8,7 +8,7 @@ const Whitepaper = () => {
             {/* White Paper */}
             <section id={styles["whitepaper-eny"]} >
                 <Title title="White Paper" sub="WhitePaper.WhitePaper.WhitePaper.WhitePaper.WhitePaper" />
-                <Link href={"/whitepaper"} passHref>
+                <Link href={"https://rocky-promotion-f0b.notion.site/ENY-Token-Whitepaper-77ec25531e2e46e59ea6ed08a4124662"} passHref>
                     <button className={styles.whiteButton}>
                         {"White Paper"}
                     </button>
